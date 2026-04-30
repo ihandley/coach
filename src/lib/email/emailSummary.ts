e76673936e1417358c062fd36d5604b6857c55ae
ianhandley <ianhandley@gmail.com>
2026-04-30 00:59:28 -0600

    72: add scan summary and replace apply flow with refresh UX

---
+++ src/lib/email/emailSummary.ts
@@ -0,0 +1,13 @@
+export interface EmailScanSummary {
+  totalScanned: number;
+  totalMatched: number;
+  totalActionable: number;
+}
+
+export function buildEmailSummary(results: any[]): EmailScanSummary {
+  return {
+    totalScanned: results.length,
+    totalMatched: results.filter(r => r.match).length,
+    totalActionable: results.filter(r => r.isActionable).length,
+  };
+}
